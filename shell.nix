0eaa8273d1211ba2a983b3cc7b728448d60de87a
Ivan Velickovic <i.velickovic@unsw.edu.au>
2024-08-13 13:06:07 +1000

    Add shell.nix
    Signed-off-by: Ivan Velickovic <i.velickovic@unsw.edu.au>

---
+++ shell.nix
@@ -0,0 +1,12 @@
+# Copyright 2024, UNSW
+#
+# SPDX-License-Identifier: BSD-2-Clause
+
+let
+    pkgs = import <nixpkgs> {};
+in
+  pkgs.mkShell {
+    buildInputs = with pkgs.buildPackages; [
+        hugo
+    ];
+}
